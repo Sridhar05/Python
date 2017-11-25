558538d2e74b9f68a0d7e89b2a96174af6c0e21c
sridhar <srigem.in@gmail.com>
2017-11-25 15:10:42 +0530

    Program to check balanced parantheses using stack

---
+++ balanced_parentheses.py
@@ -0,0 +1,28 @@
+
+"""Program to check paranthese is balanced or not"""
+
+# Importing Stack
+from stack import Stack
+
+def paranthese_checker(input_string):
+	"""pass"""
+
+	# Creating stack object
+	s = Stack()	
+
+	balanced = True
+
+	for symbol in input_string:
+		if symbol == '(':
+			s.push(symbol)
+		else:
+			if s.is_empty():
+				balanced = False
+			else:
+				s.pop()
+
+	return True if s.is_empty() and balanced else False
+
+
+print paranthese_checker('(()')
+	
